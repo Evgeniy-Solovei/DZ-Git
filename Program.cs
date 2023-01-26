6d9c85e8872f41620bb1ea63949739e0a55cd3ea
Евгений Соловей <solovey.e.v.@MacBook-Air-Evgenij.local>
2023-01-27 00:38:13 +0300

    Добавил  решение

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿string myString = "12,Apple,20,8,asde,acc";
+string[] subStrings = myString.Split(',');
+string[] Array(string[] Arr)
+{
+    int count = 0;
+    for (int i = 0; i < Arr.Length; i++)
+    {
+        if (Arr[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    string[] NewArray = new string[count];
+    for (int i = 0; i < Arr.Length; i++)
+    {
+        int a = 0;
+        if (Arr[i].Length <= 3)
+        {
+            NewArray[a] = Arr[i];
+            Console.Write($"{NewArray[a]} ");
+            a++;
+        }
+    }
+    return NewArray;
+}
+
+Array(subStrings);
